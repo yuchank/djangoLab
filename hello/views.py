5efe41cdf8a87e06fed31a1fd5b3f89ee3983fa9
yuchankwon <thexr1868@outlook.com>
2019-03-25 18:26:11 +0900

    initial codes

---
+++ hello/views.py
@@ -0,0 +1,26 @@
+import re
+from datetime import datetime
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+  return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+  now = datetime.now()
+  formatted_now = now.strftime("%A, %d %B, %Y at %X")
+  # 'Monday, 25 March, 2019 at 17:17:05'
+
+  # filter the name argument to letters only using regular expressions. 
+  # URL arguments can contain arbitrary text, so we restrict to safe characters only.
+  match_object = re.match("[a-zA-Z]+", name)
+
+  if match_object:
+    clean_name = match_object.group(0)
+  else:
+    clean_name = "Friend"
+
+  content = "Hello there, " + clean_name + "! It's " + formatted_now
+  return HttpResponse(content)
+
+print("http://127.0.0.1:8000/hello/VSCode")
