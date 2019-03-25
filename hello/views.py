b971787ff6d3a4c72a491dbbfca02d155fd0e226
thexr <thexr1868@outlook.com>
2019-03-25 23:30:28 +0900

    templates

--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,24 @@
 import re
 from datetime import datetime
+
+from django.shortcuts import render
+
 from django.http import HttpResponse
 
 # Create your views here.
+# Replace the existing home function with the one below
 def home(request):
-  return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 def hello_there(request, name):
-  now = datetime.now()
-  formatted_now = now.strftime("%A, %d %B, %Y at %X")
-  # 'Monday, 25 March, 2019 at 17:17:05'
-
-  # filter the name argument to letters only using regular expressions. 
-  # URL arguments can contain arbitrary text, so we restrict to safe characters only.
-  match_object = re.match("[a-zA-Z]+", name)
-
-  if match_object:
-    clean_name = match_object.group(0)
-  else:
-    clean_name = "Friend"
-
-  content = "Hello there, " + clean_name + "! It's " + formatted_now
-  return HttpResponse(content)
-
-print("http://127.0.0.1:8000/hello/VSCode")
+  return render(request, 'hello/hello_there.html',
+  {
+    'name': name,
+    'date': datetime.now()
+  })
