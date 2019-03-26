37dde5df607be31519ead1d819d20e3af17ae284
yuchankwon <thexr1868@outlook.com>
2019-03-26 11:06:17 +0900

    models

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,19 @@
-import re
 from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView
 
-from django.http import HttpResponse
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 # Create your views here.
-# Replace the existing home function with the one below
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+  """Renders the home page, with a list of all messages."""
+  model = LogMessage
+
+  def get_context_data(self, **kwargs):
+    context = super(HomeListView, self).get_context_data(**kwargs)
+    return context
 
 def about(request):
     return render(request, "hello/about.html")
@@ -22,3 +27,15 @@ def hello_there(request, name):
     'name': name,
     'date': datetime.now()
   })
+
+def log_message(request):
+  form = LogMessageForm(request.POST or None)
+
+  if request.method == "POST":
+    if form.is_valid():
+      message = form.save(commit=False)
+      message.log_date = datetime.now()
+      message.save()
+      return redirect("home")
+  else:
+      return render(request, "hello/log_message.html", {"form": form})
